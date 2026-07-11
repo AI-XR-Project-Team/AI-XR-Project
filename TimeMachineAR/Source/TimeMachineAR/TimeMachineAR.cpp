403f7b6d5353b509d1d05dd288703482bbac2ef9
sajul127 <rlatlgus127@gmail.com>
2026-07-11 23:36:04 +0900

    feat: 언리얼 엔진 5.4 모바일 빌드 및 플러그인 셋업 완료

---
+++ TimeMachineAR/Source/TimeMachineAR/TimeMachineAR.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TimeMachineAR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TimeMachineAR, "TimeMachineAR" );
+ 
