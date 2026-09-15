d2a8147b1f76fcfc2d3c2e6bed014f3e4a639aaa
sajul127 <rlatlgus127@gmail.com>
2026-09-16 00:56:09 +0900

    chore: Remove unused FirstPerson template files and fix C++ references

--- TimeMachineAR/Source/TimeMachineAR/TimeMachineARGameMode.cpp
+++ TimeMachineAR/Source/TimeMachineAR/TimeMachineARGameMode.cpp
@@ -7,8 +7,7 @@
 ATimeMachineARGameMode::ATimeMachineARGameMode()
 	: Super()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	// DefaultPawnClass set in Blueprint or use default
+	DefaultPawnClass = APawn::StaticClass();
 
 }
